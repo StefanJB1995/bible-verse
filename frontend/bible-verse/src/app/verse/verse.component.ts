c0a5fc2d837a67f381c6024ae4f66261ff7546e3
Stefan John Bosco <johnboscostefan@gmail.com>
2023-11-23 20:33:31 -0500

    project setup

---
+++ frontend/bible-verse/src/app/verse/verse.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verse',
+  templateUrl: './verse.component.html',
+  styleUrls: ['./verse.component.scss']
+})
+export class VerseComponent {
+
+}
