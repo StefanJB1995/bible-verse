5ac5b5a736047ce6444fa893b6e1b7a42a0e530f
Stefan John Bosco <johnboscostefan@gmail.com>
2023-11-23 20:52:48 -0500

    angular animation

--- frontend/bible-verse/src/app/verse/verse.component.ts
+++ frontend/bible-verse/src/app/verse/verse.component.ts
@@ -1,9 +1,18 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-verse',
   templateUrl: './verse.component.html',
-  styleUrls: ['./verse.component.scss']
+  styleUrls: ['./verse.component.scss'],
+  animations: [
+    trigger('slideIn', [
+      state('void', style({ transform: 'translateY(-100%)' })),
+      transition(':enter', [
+        animate('1000ms ease-out', style({ transform: 'translateY(0%)' }))
+      ])
+    ])
+  ]
 })
 export class VerseComponent {
 
