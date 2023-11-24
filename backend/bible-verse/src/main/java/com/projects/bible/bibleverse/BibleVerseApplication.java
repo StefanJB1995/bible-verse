7979d6736fe867eb932fd07261c167e0cf39c710
Stefan John Bosco <johnboscostefan@gmail.com>
2023-11-24 16:25:42 -0500

    backend setup

---
+++ backend/bible-verse/src/main/java/com/projects/bible/bibleverse/BibleVerseApplication.java
@@ -0,0 +1,13 @@
+package com.projects.bible.bibleverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibleVerseApplication.class, args);
+	}
+
+}
