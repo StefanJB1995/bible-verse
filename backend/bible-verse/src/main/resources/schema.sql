0f23bf9c8dbf4d3b9f0383c7b702b3cab09c9c6d
Stefan John Bosco <johnboscostefan@gmail.com>
2023-11-27 13:50:18 -0500

    sql

--- backend/bible-verse/src/main/resources/schema.sql
+++ backend/bible-verse/src/main/resources/schema.sql
@@ -1,3 +1,4 @@
 CREATE TABLE verse (id INT PRIMARY KEY, chapter VARCHAR(255), verse VARCHAR(4096));
+CREATE TABLE user (id INT PRIMARY KEY, username VARCHAR(255), password VARCHAR(2000));
 
 INSERT INTO verse(id,chapter,verse) VALUES (1,"John 1:1", "Testing john verse");
